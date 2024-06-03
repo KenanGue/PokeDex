a77c0b30011feb019bc4f9326589cc30f80a3831
Kenan Günes <kguenes0@gmail.com>
2024-06-03 15:13:20 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const mainPokemon_URL = "https://pokeapi.co/api/v2/pokemon/"; //Das ist die Haupt-URL
+let allPokemonData = [];//Array zum speichern aller Daten
+
+
+
+async function showPokeCard(){
+    let pokeCard = document.getElementById('poke-card');
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        
+    }
+
+}
