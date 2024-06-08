d051d36ba38879e6b1a3ef8e75122b0551a25baa
Kenan Günes <kguenes0@gmail.com>
2024-06-09 01:41:28 +0200

    Addedheadline

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
-const mainPokemon_URL = "https://pokeapi.co/api/v2/pokemon/"; //Das ist die Haupt-URL
-let allPokemonData = [];//Array zum speichern aller Daten
-
-
-
-async function showPokeCard(){
-    let pokeCard = document.getElementById('poke-card');
-    for (let index = 0; index < array.length; index++) {
-        const element = array[index];
-        
+allPokemons = [
+    {
+        "url": "https://pokeapi.co/api/v2/pokemon/",
+        "name": [],
+        "sprites.front_default": [],
+        "types[0].type.name": []
     }
+];
 
+
+async function fetchData(){
+   try {
+    let response = await fetch('https://pokeapi.co/api/v2/pokemon/1');
+    let data = await response.json();
+showPokeCard(data);
+    console.log(data);
+   } catch (error) {
+    console.error('Fehler beim abrufen der API', error);
+   }
 }
+
+function showPokeCard(data) {
+    let pokeCard = document.getElementById('poke-card');
+let name = data.name;
+let img = data.sprites.front_default;
+let type = data.types[0].type.name;
+    pokeCard.innerHTML = `
+                <div class="poke-item">
+                    <p>${name}</p>
+                    <img src="${img}">
+                    <p>${type}</p>
+                </div>
+                 `;
+}
+
