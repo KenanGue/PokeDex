d7bf32478c6a09c4ff41fcb68076ddcde757779b
Kenan Günes <kguenes0@gmail.com>
2024-06-12 09:01:57 +0200

    processed

--- script.js
+++ script.js
@@ -1,35 +1,40 @@
-allPokemons = [
-    {
-        "url": "https://pokeapi.co/api/v2/pokemon/",
-        "name": [],
-        "sprites.front_default": [],
-        "types[0].type.name": []
-    }
-];
-
-
-async function fetchData(){
-   try {
-    let response = await fetch('https://pokeapi.co/api/v2/pokemon/1');
+async function fetchData() {
+    let response = await fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=250');
     let data = await response.json();
-showPokeCard(data);
     console.log(data);
-   } catch (error) {
-    console.error('Fehler beim abrufen der API', error);
-   }
+    showPokeCard(data.results);
 }
 
-function showPokeCard(data) {
+
+async function showPokeCard(pokemons) {
     let pokeCard = document.getElementById('poke-card');
-let name = data.name;
-let img = data.sprites.front_default;
-let type = data.types[0].type.name;
-    pokeCard.innerHTML = `
-                <div class="poke-item">
-                    <p>${name}</p>
-                    <img src="${img}">
-                    <p>${type}</p>
-                </div>
-                 `;
+    pokeCard.innerHTML = '';
+
+    for (let i = 0; i < pokemons.length; i++) {
+        let pokemon = pokemons[i];
+        let response = await fetch(pokemon.url);
+        let pokeData = await response.json();
+
+        pokeCard.innerHTML += await loadHTML(pokemon, pokeData);
+    }
 }
 
+function loadHTML(pokemon, pokeData) {
+
+    let pokeName = pokeData.name.charAt(0).toUpperCase() + pokeData.name.slice(1);
+    let pokeImg = pokeData.sprites.other['official-artwork'].front_default;
+    let pokeTypes = pokeData.types.map(typeInfo => typeInfo.type.name);
+    let mainType = pokeTypes[0];
+    let typeBackgroundColor = pokeTypes.map(type => `<p class="type-border">${type}</p>`).join('');
+
+    return `
+    <div class="poke-item type-${mainType}">
+        <h6>${pokeName}</h6>
+        <img src="${pokeImg}" class="poke-image">
+       <div class="type-position">
+        <p>${typeBackgroundColor}</p>
+        </div>
+    </div>
+    
+    `;
+}
